f0ea6c73823d4af4f6cc9a73c761188ad98e1fb8
Aman <cheersaman1@gmail.com>
2026-08-30 14:50:16 +0530

    chore: clean up stray test scripts and generator debris

--- test.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-fetch('http://localhost:8811/sse').then(res => {
-    const sessionId = res.headers.get('set-cookie')?.split('sessionId=')[1]?.split(';')[0];
-    return fetch('http://localhost:8811/message', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json', 'Cookie': 'sessionId=' + sessionId },
-        body: JSON.stringify({ method: 'tools/call', params: { name: 'get_contract_source', arguments: {} } })
-    });
-}).then(res => res.text()).then(console.log);
